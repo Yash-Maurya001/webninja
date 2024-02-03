857bbbab99752753aed7dc4a66b901533adeaf94
Yash Maurya <yashmaurya.in@gmail.com>
2024-02-03 21:21:02 +0530

    Update Navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ function NavBar() {
 	return (
 		<>
 			<div className="w-full flex justify-center p-2  bg-blue-300">
-				<nav className="flex gap-4 list-none font-mono font-semibold text-xl cursor-pointer">
+				<nav className=" pl-[70px] flex gap-4 list-none font-mono font-semibold text-xl cursor-pointer">
 					<li>Home</li>
 					<li>About</li>
 					<li>Community</li>
